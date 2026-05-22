21e7db8820e6e6ab0c9fe87aa6b5eff73396b24d
unknown <akshaymenon315@gmail.com>
2026-05-22 12:03:57 +0530

    first function add

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from .models import todo
+from .serializer import task_serializer
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view(['GET'])
+def todo_list(request):
+    a=todo.objects.all()
+    b= task_serializer(a,many=True)
+    return Response(b.data)
